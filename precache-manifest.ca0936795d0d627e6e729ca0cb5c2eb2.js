80e6d545f9a3f59c51501dbc2f901beb941b96b0
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-07-01 20:57:05 +0530

    Updates

---
+++ precache-manifest.ca0936795d0d627e6e729ca0cb5c2eb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3571ce070446cb5d13480ef50a599080",
+    "url": "/educateYourself/index.html"
+  },
+  {
+    "revision": "b0e713214f92904f1c02",
+    "url": "/educateYourself/static/css/main.e4487ea5.chunk.css"
+  },
+  {
+    "revision": "706250ca0d3a50db4c2d",
+    "url": "/educateYourself/static/js/2.c3b992e9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/educateYourself/static/js/2.c3b992e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e713214f92904f1c02",
+    "url": "/educateYourself/static/js/main.8c9d938d.chunk.js"
+  },
+  {
+    "revision": "83ed7fc12ad48850e8db",
+    "url": "/educateYourself/static/js/runtime-main.c0949d28.js"
+  },
+  {
+    "revision": "5ec8e508f07ceab62276df416b8fc5bd",
+    "url": "/educateYourself/static/media/image.5ec8e508.jpg"
+  }
+]);
